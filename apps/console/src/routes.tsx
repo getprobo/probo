f2e20d6e3b0bb1b7f738b100fa08d675457cccf8
Émile Ré <nemile.re@gmail.com>
2025-12-24 11:15:31 +0100

    Replug SAML configurations
    Signed-off-by: Émile Ré <nemile.re@gmail.com>

--- apps/console/src/routes.tsx
+++ apps/console/src/routes.tsx
@@ -218,12 +218,13 @@ const routes = [
               () => import("./pages/organizations/settings/DomainSettingsTab"),
             ),
           },
-          // {
-          //   path: "saml-sso",
-          //   Component: lazy(
-          //     () => import("./pages/organizations/settings/SAMLSettingsTab")
-          //   ),
-          // },
+          {
+            path: "saml-sso",
+            Component: lazy(
+              () =>
+                import("./pages/iam/organizations/settings/SAMLSettingsPageLoader"),
+            ),
+          },
         ],
       },
       ...riskRoutes,
