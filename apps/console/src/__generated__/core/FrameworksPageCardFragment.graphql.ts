b54d9bf6939d9777b0007bf9b2a168126a9957cb
Émile Ré <nemile.re@gmail.com>
2025-12-31 17:49:56 +0100

    Fix framework pages permissions handling
    Signed-off-by: Émile Ré <nemile.re@gmail.com>

--- apps/console/src/__generated__/core/FrameworksPageCardFragment.graphql.ts
+++ apps/console/src/__generated__/core/FrameworksPageCardFragment.graphql.ts
@@ -1,5 +1,5 @@
 /**
- * @generated SignedSource<<412cc494e90709fca575c68f6deef112>>
+ * @generated SignedSource<<d797330ab78ac421e3c64cebdb2ec975>>
  * @lightSyntaxTransform
  * @nogrep
  */
@@ -11,6 +11,8 @@
 import { ReaderFragment } from 'relay-runtime';
 import { FragmentRefs } from "relay-runtime";
 export type FrameworksPageCardFragment$data = {
+  readonly canDelete: boolean;
+  readonly canUpdate: boolean;
   readonly darkLogoURL: string | null | undefined;
   readonly description: string | null | undefined;
   readonly id: string;
@@ -63,12 +65,38 @@ const node: ReaderFragment = {
       "kind": "ScalarField",
       "name": "darkLogoURL",
       "storageKey": null
+    },
+    {
+      "alias": "canUpdate",
+      "args": [
+        {
+          "kind": "Literal",
+          "name": "action",
+          "value": "core:framework:update"
+        }
+      ],
+      "kind": "ScalarField",
+      "name": "permission",
+      "storageKey": "permission(action:\"core:framework:update\")"
+    },
+    {
+      "alias": "canDelete",
+      "args": [
+        {
+          "kind": "Literal",
+          "name": "action",
+          "value": "core:framework:delete"
+        }
+      ],
+      "kind": "ScalarField",
+      "name": "permission",
+      "storageKey": "permission(action:\"core:framework:delete\")"
     }
   ],
   "type": "Framework",
   "abstractKey": null
 };
 
-(node as any).hash = "fe86a14971741a77f587ae015ebf0a22";
+(node as any).hash = "dfb64192cedec377c8df8a6d8251e815";
 
 export default node;
