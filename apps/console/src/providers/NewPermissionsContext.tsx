0c18e8a7c153308e725495574437b6053c9de94c
Émile Ré <nemile.re@gmail.com>
2026-01-02 14:03:47 +0100

    Get rid of previous permission providers
    Signed-off-by: Émile Ré <nemile.re@gmail.com>

--- apps/console/src/providers/NewPermissionsContext.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-export type PermissionsContextType = Record<string, boolean>;
-
-export const PermissionsContext = createContext<PermissionsContextType>({});
