3c1017574e9d8d00e1fd6bdd3e0681abd6e342df
Yash Gupta <yash3144@gmail.com>
2026-01-11 02:52:58 +0530

    fix: normalize domain/email columns with citext
    Signed-off-by: Yash Gupta <yash3144@gmail.com>

---
+++ pkg/coredata/migrations/20260111T211335Z.sql
@@ -0,0 +1,8 @@
+ALTER TABLE auth_saml_configurations
+    ALTER COLUMN email_domain TYPE CITEXT;
+
+ALTER TABLE peoples
+    ALTER COLUMN primary_email_address TYPE CITEXT;
+
+ALTER TABLE peoples
+    ALTER COLUMN additional_email_addresses TYPE CITEXT[];
