c24726b481c9771f51e2bf5048fa261bb838418c
Yash Gupta <yash3144@gmail.com>
2026-01-14 14:44:51 +0530

    chore: add shared ESLint and Prettier configuration
    Closes #615
    
    - Add @probo/eslint-config package with ESLint 9.x flat config
    - Enhance @probo/prettier with aligned stylistic rules
    - Migrate console, trust, and UI packages to shared config
    - Remove duplicate ESLint dependencies from individual packages
    
    Signed-off-by: Yash Gupta <yash3144@gmail.com>

--- packages/prettier/prettier.config.js
+++ packages/prettier/prettier.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   tabWidth: 2,
+  useTabs: false,
+  semi: true, // Add semicolons
+  singleQuote: false, // Use double quotes (matches @stylistic/quotes)
+  quoteProps: "as-needed",
+  trailingComma: "all", // Trailing commas (matches @stylistic/comma-dangle)
+  bracketSpacing: true, // { foo: bar } not {foo: bar}
+  arrowParens: "always", // (x) => x not x => x
+  endOfLine: "lf", // Unix line endings
+  printWidth: 120, // Max line length (matches @stylistic/max-len)
+  proseWrap: "preserve",
+  bracketSameLine: false,
 };
