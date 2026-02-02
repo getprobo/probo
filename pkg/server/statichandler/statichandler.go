150ff353e2ff4d955bbf1d87f84503b837c6ef81
Bryan Frimin <bryan@getprobo.com>
2026-02-02 11:49:11 +0100

    Remove deprecated http header fields
    Signed-off-by: Bryan Frimin <bryan@getprobo.com>

--- pkg/server/statichandler/statichandler.go
+++ pkg/server/statichandler/statichandler.go
@@ -134,8 +134,6 @@ func (s *Server) ServeSPA(w http.ResponseWriter, r *http.Request) {
 		}
 
 		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
-		w.Header().Set("Pragma", "no-cache")
-		w.Header().Set("Expires", "0")
 
 		w.WriteHeader(http.StatusOK)
 		w.Write(s.indexContent)
