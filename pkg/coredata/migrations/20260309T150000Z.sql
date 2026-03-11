f99b2f782822507e8c3126a16283271248401ea0
Bryan Frimin <bryan@getprobo.com>
2026-03-11 11:53:27 +0100

    Rename nickname
    Signed-off-by: Bryan Frimin <bryan@getprobo.com>

--- pkg/coredata/migrations/20260309T150000Z.sql
+++ pkg/coredata/migrations/20260309T150000Z.sql
@@ -1,6 +1,6 @@
 ALTER TABLE iam_membership_profiles
     ALTER COLUMN kind TYPE TEXT USING kind::TEXT,
-    ADD COLUMN nick_name TEXT,
+    ADD COLUMN nickname TEXT,
     ADD COLUMN locale TEXT,
     ADD COLUMN timezone TEXT,
     ADD COLUMN profile_url TEXT;
