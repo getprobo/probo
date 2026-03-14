175afbfc0f935ef3238485bc69f43b66be7de064
Bryan Frimin <bryan@getprobo.com>
2026-03-14 15:34:44 +0100

    Add proboctl entry point and root command
    Wire up all command groups into the root command: api, auth,
    completion, risk, and version. Add --no-interactive persistent
    flag and PROBO_NO_INTERACTIVE env var support. Add shell
    completion generation for bash, zsh, fish, and powershell.
    
    Signed-off-by: Bryan Frimin <bryan@getprobo.com>

---
+++ cmd/proboctl/main.go
@@ -0,0 +1,63 @@
+// Copyright (c) 2026 Probo Inc <hello@getprobo.com>.
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+// OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"go.probo.inc/probo/pkg/cli/api"
+	"go.probo.inc/probo/pkg/cli/config"
+	"go.probo.inc/probo/pkg/cmd/cmdutil"
+	"go.probo.inc/probo/pkg/cmd/iostreams"
+	"go.probo.inc/probo/pkg/cmd/root"
+)
+
+var version string = "unknown"
+
+func main() {
+	ios := iostreams.System()
+	if v := os.Getenv("PROBO_NO_INTERACTIVE"); v == "1" || v == "true" {
+		ios.ForceNonInteractive = true
+	}
+
+	f := &cmdutil.Factory{
+		IOStreams: ios,
+		Version:  version,
+		Config: func() (*config.Config, error) {
+			return config.Load()
+		},
+		APIClient: func() (*api.Client, error) {
+			cfg, err := config.Load()
+			if err != nil {
+				return nil, err
+			}
+
+			host, hc, err := cfg.DefaultHost()
+			if err != nil {
+				return nil, err
+			}
+
+			return api.NewClient(host, hc.Token), nil
+		},
+	}
+
+	cmd := root.NewCmdRoot(f)
+
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
+	}
+}
---
+++ pkg/cmd/root/root.go
@@ -0,0 +1,54 @@
+// Copyright (c) 2026 Probo Inc <hello@getprobo.com>.
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+// OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
+package root
+
+import (
+	"github.com/spf13/cobra"
+	cmdapi "go.probo.inc/probo/pkg/cmd/api"
+	"go.probo.inc/probo/pkg/cmd/auth"
+	"go.probo.inc/probo/pkg/cmd/cmdutil"
+	"go.probo.inc/probo/pkg/cmd/completion"
+	"go.probo.inc/probo/pkg/cmd/risk"
+	"go.probo.inc/probo/pkg/cmd/version"
+)
+
+func NewCmdRoot(f *cmdutil.Factory) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:           "proboctl <command> [flags]",
+		Short:         "Probo CLI",
+		Long:          "proboctl is a command-line tool for interacting with the Probo platform.",
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			if noInteractive, _ := cmd.Flags().GetBool("no-interactive"); noInteractive {
+				f.IOStreams.ForceNonInteractive = true
+			}
+		},
+	}
+
+	cmd.PersistentFlags().Bool(
+		"no-interactive",
+		false,
+		"Disable interactive prompts (also set via PROBO_NO_INTERACTIVE=1)",
+	)
+
+	cmd.AddCommand(cmdapi.NewCmdAPI(f))
+	cmd.AddCommand(auth.NewCmdAuth(f))
+	cmd.AddCommand(completion.NewCmdCompletion(f))
+	cmd.AddCommand(risk.NewCmdRisk(f))
+	cmd.AddCommand(version.NewCmdVersion(f))
+
+	return cmd
+}
