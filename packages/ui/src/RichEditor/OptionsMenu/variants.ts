cc933789969fe610c65d09232338daffbdc218ea
Émile Ré <emile@getprobo.com>
2026-03-26 10:48:16 +0400

    Split menu components for better responsibility handling
    Signed-off-by: Émile Ré <emile@getprobo.com>

---
+++ packages/ui/src/RichEditor/OptionsMenu/variants.ts
@@ -0,0 +1,15 @@
+// Copyright (c) 2026 Probo Inc <hello@getprobo.com>.
+// Use of this source code is governed by the ISC license
+// that can be found in the LICENSE file.
+
+import { tv } from "tailwind-variants";
+
+export const optionsMenuVariants = tv({
+  slots: {
+    trigger: [
+      "z-10 flex size-6 items-center justify-center",
+      "rounded text-txt-tertiary hover:bg-subtle hover:text-txt-primary cursor-grab",
+    ],
+    menu: ["rounded-lg border border-border-mid bg-level-0 p-1 shadow-md z-20"],
+  },
+});
