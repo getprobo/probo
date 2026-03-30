f18fb34d61c069f1d21c8fdda8f64a5fe912e722
Sacha Al Himdani <sacha@getprobo.com>
2026-03-30 18:28:04 +0200

    Add ISC license headers to Go, TypeScript, and SQL files
    Add ISC license headers to all .go, .ts, .tsx, and .sql files
    using each file's git history to determine the correct copyright
    year or year range.
    
    Signed-off-by: Sacha Al Himdani <sacha@getprobo.com>

--- apps/console/src/pages/organizations/employee/EmployeeTabsLayout.tsx
+++ apps/console/src/pages/organizations/employee/EmployeeTabsLayout.tsx
@@ -1,9 +1,48 @@
+// Copyright (c) 2026 Probo Inc <hello@getprobo.com>.
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+// OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
 import { useTranslate } from "@probo/i18n";
 import { PageHeader, TabLink, Tabs } from "@probo/ui";
+import { useLazyLoadQuery } from "react-relay";
+import { graphql } from "react-relay";
 import { Outlet } from "react-router";
+
+import type { EmployeeTabsLayoutQuery } from "#/__generated__/core/EmployeeTabsLayoutQuery.graphql";
+import { useOrganizationId } from "#/hooks/useOrganizationId";
+
+const employeeTabsLayoutQuery = graphql`
+  query EmployeeTabsLayoutQuery($organizationId: ID!) {
+    viewer @required(action: THROW) {
+      approvableDocuments(organizationId: $organizationId, first: 1) {
+        edges {
+          __typename
+        }
+      }
+    }
+  }
+`;
 
 export default function EmployeeTabsLayout() {
   const { __ } = useTranslate();
+  const organizationId = useOrganizationId();
+
+  const data = useLazyLoadQuery<EmployeeTabsLayoutQuery>(
+    employeeTabsLayoutQuery,
+    { organizationId },
+  );
+
+  const hasApprovals = data.viewer.approvableDocuments.edges.length > 0;
 
   return (
     <div className="space-y-6">
@@ -12,9 +51,11 @@ export default function EmployeeTabsLayout() {
         <TabLink to="signatures" end>
           {__("Signatures")}
         </TabLink>
-        <TabLink to="approvals" end>
-          {__("Approvals")}
-        </TabLink>
+        {hasApprovals && (
+          <TabLink to="approvals" end>
+            {__("Approvals")}
+          </TabLink>
+        )}
       </Tabs>
       <Outlet />
     </div>
