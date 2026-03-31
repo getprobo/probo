851e585b9b288fe8b861711fcd7bc1db6caddf23
Sacha Al Himdani <sacha@getprobo.com>
2026-03-31 11:21:35 +0200

    Add ISC license headers to Go, TypeScript, and SQL files
    Add ISC license headers to all .go, .ts, .tsx, and .sql files
    using each file's git history to determine the correct copyright
    year or year range. Trademarked icons (brand logos, vendor logos,
    compliance framework logos) are excluded.
    
    Signed-off-by: Sacha Al Himdani <sacha@getprobo.com>

--- pkg/coredata/migrations/20250420T120000Z.sql
+++ pkg/coredata/migrations/20250420T120000Z.sql
@@ -1,3 +1,17 @@
+-- Copyright (c) 2025-2026 Probo Inc <hello@getprobo.com>.
+--
+-- Permission to use, copy, modify, and/or distribute this software for any
+-- purpose with or without fee is hereby granted, provided that the above
+-- copyright notice and this permission notice appear in all copies.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+-- REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+-- AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+-- INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+-- LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+-- OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+-- PERFORMANCE OF THIS SOFTWARE.
+
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 -- PostgreSQL implementation of the GID generation system
@@ -303,4 +317,3 @@ JOIN users_organizations uo ON u.id = uo.user_id
 JOIN organizations o ON uo.organization_id = o.id
 LEFT JOIN peoples p ON u.email_address = p.primary_email_address
 WHERE p.id IS NULL;
-
