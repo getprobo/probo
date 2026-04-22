72443ff32f16a7caecaff78f614d34c47e07d17e
Émile Ré <emile@getprobo.com>
2026-04-22 15:49:56 +0400

    Use full Probo logo (picto + wordmark) in cookie banner branding
    Replace the picto-only SVG with the full Logo component in both the
    console theme preview and the cookie-banner web component.
    
    Signed-off-by: Émile Ré <emile@getprobo.com>

--- apps/console/src/pages/organizations/cookie-banners/configuration/snippet/_components/ThemePreview.tsx
+++ apps/console/src/pages/organizations/cookie-banners/configuration/snippet/_components/ThemePreview.tsx
@@ -13,7 +13,7 @@
 // PERFORMANCE OF THIS SOFTWARE.
 
 import { useTranslate } from "@probo/i18n";
-import { Button, Card, Field, Input, useToast } from "@probo/ui";
+import { Button, Card, Field, Input, Logo, useToast } from "@probo/ui";
 import { useCallback, useMemo, useState } from "react";
 import { useFragment } from "react-relay";
 import { graphql } from "relay-runtime";
@@ -302,25 +302,7 @@ function BannerPreview({ showBranding }: { showBranding: boolean }) {
         >
           Privacy by
           {" "}
-          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 25 24" fill="none" aria-hidden="true" style={{ verticalAlign: "-2px", display: "inline" }}>
-            <g clipPath="url(#pc-preview)">
-              <rect width="24" height="24" x=".85" fill="currentColor" rx="5.14" />
-              <path stroke="#fff" strokeWidth="3.53" d="M20.57 6.17c2.96 4.47-6.43 13.97-20.32 5.66" />
-              <path stroke="url(#pg-preview)" strokeWidth="3.53" d="M20.57 6.17c2.96 4.47-6.43 13.97-20.32 5.66" />
-              <path fill="#fff" d="M19.07 7.1a1.77 1.77 0 0 0 3-1.86l-1.5.93-1.5.94ZM8.82 25.18l1.71-.41c-2.3-9.67.01-14.66 2.54-16.83A6.25 6.25 0 0 1 17 6.33c1.22-.01 1.87.44 2.08.78l1.5-.94 1.5-.93c-1.1-1.74-3.16-2.46-5.13-2.44-2.03.03-4.26.81-6.17 2.45-3.9 3.35-6.16 9.92-3.67 20.33l1.72-.41Z" />
-            </g>
-            <defs>
-              <linearGradient id="pg-preview" x1="18.77" x2="1.2" y1="13.56" y2="13.56" gradientUnits="userSpaceOnUse">
-                <stop stopColor="#101E1C" stopOpacity="0" />
-                <stop offset="1" stopColor="#fff" />
-              </linearGradient>
-              <clipPath id="pc-preview">
-                <rect width="24" height="24" x=".85" fill="#fff" rx="5.14" />
-              </clipPath>
-            </defs>
-          </svg>
-          {" "}
-          Probo
+          <Logo withPicto className="inline h-3.5 align-[-3px]" />
         </div>
       )}
     </div>
