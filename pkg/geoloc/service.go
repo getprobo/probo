245b9ef7c11e0f51a161ca666769c958b04aa3ac
Émile Ré <emile@getprobo.com>
2026-05-07 15:57:46 +0400

    Fix lint
    Signed-off-by: Émile Ré <emile@getprobo.com>

--- pkg/geoloc/service.go
+++ pkg/geoloc/service.go
@@ -162,7 +162,7 @@ func parseCIDRFile(path string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer f.Close()
+	defer func() { _ = f.Close() }()
 
 	var cidrs []string
 	scanner := bufio.NewScanner(f)
