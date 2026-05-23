1eba43f96f6779a65aa2a7976cf47418657f4695
Bryan Frimin <bryan@probo.com>
2026-05-23 13:37:33 -0700

    Style
    Signed-off-by: Bryan Frimin <bryan@probo.com>

--- pkg/iam/authorizer_unit_test.go
+++ pkg/iam/authorizer_unit_test.go
@@ -354,7 +354,6 @@ func TestAuthorizer_BuildAuditLogEntry(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
--- pkg/iam/errors_test.go
+++ pkg/iam/errors_test.go
@@ -104,7 +104,6 @@ func TestAuthorizeRelatedErrors_Error(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			if tt.err.Error() != tt.want {
