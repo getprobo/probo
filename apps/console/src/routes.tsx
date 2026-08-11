zaid3/compplus-

Add compliance manager membership role
Owners and admins can assign COMPLIANCE_MANAGER so people
can fully manage the compliance portal without broader
admin access. Grant portal full access plus the related
document, audit, and third-party visibility permissions.

Signed-off-by: Bryan Frimin <bryan@probo.com>

Co-authored-by: Bryan FRIMIN <bryan@frimin.fr>
Signed-off-by: Cursor Agent <cursoragent@cursor.com>

apps/console/src/routes.tsx
@@ -273,9 +273,11 @@ const routes = [
                   return <Navigate to="employee" />;
                 case Role.AUDITOR:
                   return <Navigate to="measures" />;
+                case Role.COMPLIANCE_MANAGER:
+                  return <Navigate to="compliance-page" />;
                 default:
                   return <Navigate to="tasks" />;
-              }
+                }
             },
           },
           {

getprobo/probo

apps/console/src/routes.tsx
@@ -275,9 +275,11 @@ const routes = [
                   return <Navigate to="employee" />;
                 case Role.AUDITOR:
                   return <Navigate to="measures" />;
+                case Role.COMPLIANCE_MANAGER:
+                  return <Navigate to="compliance-portals" />;
                 default:
                   return <Navigate to="tasks" />;
-              }
+                }
             },
           },
           {